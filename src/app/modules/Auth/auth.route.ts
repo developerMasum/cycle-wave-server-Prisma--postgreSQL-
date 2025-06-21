4702762b64b2aee2287f1b3f130b6cd39f8a1de4
developerMasum <developer.masum2001@gmail.com>
2025-06-21 12:08:20 +0600

    added auth

---
+++ src/app/modules/Auth/auth.route.ts
@@ -0,0 +1,19 @@
+import express, { NextFunction, Request, Response } from "express";
+import { AuthController } from "./auth.controller";
+
+const router = express.Router();
+
+router.post("/login", AuthController.loginUser);
+router.post(
+  "/refresh-token",
+  //   validateRequest(AuthValidation.refreshTokenZodSchema),
+  AuthController.refreshToken
+);
+
+router.post(
+  "/change-password",
+
+  AuthController.changePassword
+);
+
+export const AuthRoutes = router;
